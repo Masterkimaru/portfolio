792b7b0da40fcdcfaa3211c98f54f5b699eedb05
Masterkimaru <themasterskimaru@gmail.com>
2026-02-18 21:51:40 +0300

    new changes for this Target.jsx file code

--- src/components/Target.jsx
+++ src/components/Target.jsx
@@ -4,9 +4,6 @@ import gsap from 'gsap';
 
 const Target = (props) => {
   const targetRef = useRef();
-  const { scene } = useGLTF(
-    'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf',
-  );
 
   useGSAP(() => {
     gsap.to(targetRef.current.position, {
