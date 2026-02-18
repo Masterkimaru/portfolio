4d1e298b078367d73ede7beca23395b9ccac091d
Masterkimaru <themasterskimaru@gmail.com>
2026-02-18 21:48:30 +0300

    New changes for the target.jsx file code

--- src/components/Target.jsx
+++ src/components/Target.jsx
@@ -1,13 +1,9 @@
-import { useGLTF } from '@react-three/drei';
 import { useRef } from 'react';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 
 const Target = (props) => {
   const targetRef = useRef();
-  const { scene } = useGLTF(
-    'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf',
-  );
 
   useGSAP(() => {
     gsap.to(targetRef.current.position, {
@@ -19,9 +15,41 @@ const Target = (props) => {
   });
 
   return (
-    <mesh {...props} ref={targetRef} rotation={[0, Math.PI / 5, 0]} scale={1.5}>
-      <primitive object={scene} />
-    </mesh>
+    <group {...props} ref={targetRef} rotation={[0, Math.PI / 5, 0]} scale={1.5}>
+      {/* Outer red ring */}
+      <mesh position={[0, 0, 0]}>
+        <cylinderGeometry args={[1.2, 1.2, 0.1, 32]} />
+        <meshStandardMaterial color="#ff0000" />
+      </mesh>
+      
+      {/* White ring */}
+      <mesh position={[0, 0, 0.05]}>
+        <cylinderGeometry args={[0.9, 0.9, 0.1, 32]} />
+        <meshStandardMaterial color="#ffffff" />
+      </mesh>
+      
+      {/* Inner red ring */}
+      <mesh position={[0, 0, 0.1]}>
+        <cylinderGeometry args={[0.6, 0.6, 0.1, 32]} />
+        <meshStandardMaterial color="#ff0000" />
+      </mesh>
+      
+      {/* Bullseye (yellow center) */}
+      <mesh position={[0, 0, 0.15]}>
+        <cylinderGeometry args={[0.3, 0.3, 0.1, 32]} />
+        <meshStandardMaterial color="#ffdd00" emissive="#ffaa00" emissiveIntensity={0.2} />
+      </mesh>
+
+      {/* Stand/Legs */}
+      <mesh position={[-0.8, -1.2, 0]} rotation={[0, 0, Math.PI / 6]}>
+        <boxGeometry args={[0.1, 1.5, 0.1]} />
+        <meshStandardMaterial color="#8B4513" />
+      </mesh>
+      <mesh position={[0.8, -1.2, 0]} rotation={[0, 0, -Math.PI / 6]}>
+        <boxGeometry args={[0.1, 1.5, 0.1]} />
+        <meshStandardMaterial color="#8B4513" />
+      </mesh>
+    </group>
   );
 };
 
