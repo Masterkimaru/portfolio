5ddaf89bac6f710d512afc06fa02f96ab208d5f5
Wachira Kelvin <83416718+Masterkimaru@users.noreply.github.com>
2026-02-18 21:37:41 +0300

    Comment out GLTF model loading in Target.jsx
    Comment out the GLTF model loading code for Target component.

--- src/components/Target.jsx
+++ src/components/Target.jsx
@@ -5,9 +5,9 @@ import gsap from 'gsap';
 
 const Target = (props) => {
   const targetRef = useRef();
-  const { scene } = useGLTF(
+  /*const { scene } = useGLTF(
     'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf',
-  );
+  ); */
 
   useGSAP(() => {
     gsap.to(targetRef.current.position, {
